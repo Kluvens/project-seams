ca35e9cc5a80d1f33f9e8fc8b3fa41b684b5e13c
Kais Alzubaidi <k.alzubaidi@student.unsw.edu.au>
2022-03-01 02:25:53 +1100

    Added a new test, improved code quality/style (pylint 8.2)

--- tests/test_auth_login.py
+++ tests/test_auth_login.py
@@ -1,16 +1,16 @@
-import pytest
 from src.error import InputError
 from src.auth import auth_login_v1
 from src.auth import auth_register_v1
-from src.data_store import data_store
 from src.other import clear_v1
 
-'''
-Version 1.0: Tests for auth_login_v1
-- K.Al
-'''
+import pytest
 
-# =============================TESTING CORRECTNESS============================
+
+#Version 1.1: Tests for auth_login_v1
+# Kais.Al
+
+
+# =============================TESTING CORRECTNESS=============================
 
 # Test if it can the return the correct user id for an exisiting user when
 # loggin in 
@@ -20,7 +20,7 @@ def test_login_existing_user1():
     password = "hello12344"
     uid = auth_register_v1(email, password, "James", "Smith")
     return_uid = auth_login_v1(email, password)
-    assert(return_uid == uid)
+    assert return_uid == uid
 
 # Same test to the above with minor change to the email parameter
 def test_login_existing_user2():
@@ -29,10 +29,27 @@ def test_login_existing_user2():
     password = "hello12344"
     uid = auth_register_v1(email, password, "James", "Smith")
     return_uid = auth_login_v1(email, password)
-    assert(return_uid == uid)
+    assert return_uid == uid
 
 
-# ============================ TESTING EXCEPTIONS ============================
+# Testing behaviour when there are a number of exisiting users in databse
+def test_login_existing_user3():
+    clear_v1()
+    auth_register_v1("k11@gmail.com", "Password123", "Casey1", "Roland")
+    auth_register_v1("k22@gmail.com", "Password123", "Casey2", "Roland")
+
+    email = "james123@hotmai.com"
+    password = "123james123"
+    uid = auth_register_v1(email, password, "James", "Smith")
+
+    auth_register_v1("k33@gmail.com", "Password123", "Casey3", "Roland")
+    auth_register_v1("k44@gmail.com", "Password123", "Casey4", "Roland")
+
+
+    return_uid = auth_login_v1(email, password)
+    assert return_uid == uid
+
+# ============================ TESTING EXCEPTIONS =============================
 
 def test_does_email_exit1():
     clear_v1()
@@ -50,15 +67,15 @@ def test_does_email_exit2():
 def test_is_password_correct1():
     clear_v1()
     email = "k123@gmail.com"
-    uid = auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
+    auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
     with pytest.raises(InputError):
         auth_login_v1(email, "HELLOHELLO123")
 
-# incorrect password containing the correct password as a substring 
+# incorrect password containing the correct password as a substring
 def test_is_password_correct2():
     clear_v1()
     email = "k123@gmail.com"
-    uid = auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
+    auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
     with pytest.raises(InputError):
         auth_login_v1(email, "ABc123HIIIIIIII")
 
@@ -66,24 +83,24 @@ def test_is_password_correct2():
 def test_is_password_correct3():
     clear_v1()
     email = "k123@gmail.com"
-    uid = auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
+    auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
     with pytest.raises(InputError):
         auth_login_v1(email, "ABc123HIIIII")
 
-# Incorrect password that is identical to the correct password 
+# Incorrect password that is identical to the correct password
 # but with lowercases only
 def test_is_password_correct4():
     clear_v1()
     email = "k123@gmail.com"
-    uid = auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
+    auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
     with pytest.raises(InputError):
         auth_login_v1(email, "abc123hiiiiii")
 
-# Incorrect password that is identical to the correct password but with one 
+# Incorrect password that is identical to the correct password but with one
 # character being lowercase instead of uppercase
 def test_is_password_correct5():
     clear_v1()
     email = "k123@gmail.com"
-    uid = auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
+    auth_register_v1(email, "ABc123HIIIIII", "John", "Smith")
     with pytest.raises(InputError):
         auth_login_v1(email, "ABc123hIIIIII")

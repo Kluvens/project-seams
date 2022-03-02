e5e0a2dbad1101f29522e2659bce7dca8aabcc39
James Cai <james.cai@student.unsw.edu.au>
2022-03-02 22:38:17 +1100

    added working tests for listall and list

---
+++ tests/channels_test.py
@@ -0,0 +1,101 @@
+import pytest
+
+from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
+from src.other import clear_v1
+from src.data_store import data_store
+from src.error import InputError
+from src.auth import auth_register_v1
+
+# =============================TESTING CORRECTNESS============================
+
+# test if list func returns None when no channels joined
+def test_no_channels_joined_public():
+    clear_v1()
+
+    # create two users
+    uid1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    uid2 = auth_register_v1("e2@gmail.com", "abcdefg123", "Jam", "Cao")
+
+    # create channel 
+    cid1 = channels_create_v1(uid1['auth_user_id'], "ch1", True)
+
+    listv1 = channels_list_v1(uid2['auth_user_id'])
+
+    # pass if trying to access non-existant channel
+    with pytest.raises(IndexError):
+        listv1['channels'][uid2['auth_user_id']]
+
+def test_no_channels_joined_private():
+    clear_v1()
+
+    # create two users
+    uid1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    uid2 = auth_register_v1("e2@gmail.com", "abcdefg123", "Jam", "Cao")
+
+    # create channel 
+    cid1 = channels_create_v1(uid1['auth_user_id'], "ch1", False)
+
+    listv1 = channels_list_v1(uid2['auth_user_id'])
+
+    # pass if trying to access non-existant channel
+    with pytest.raises(IndexError):
+        listv1['channels'][uid2['auth_user_id']]
+
+# test if user has joined all channels 
+def test_all_channels_joined():
+    clear_v1()
+
+    # create user
+    uid1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+
+    # create channels 
+    cid1 = channels_create_v1(uid1['auth_user_id'], "ch1", True)
+    cid2 = channels_create_v1(uid1['auth_user_id'], "ch2", True)
+
+    assert channels_list_v1(uid1['auth_user_id']) == channels_listall_v1(uid1['auth_user_id'])  
+
+# test if user has joined some channels
+def test_user_join_some_public():
+    clear_v1()
+
+    # create users
+    uid1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    uid2 = auth_register_v1("e2@gmail.com", "abcdefg123", "Jam", "Cao")
+
+    # create channels
+    cid1 = channels_create_v1(uid1['auth_user_id'], "ch1", True)
+    cid2 = channels_create_v1(uid1['auth_user_id'], "ch2", True)
+
+    # join user_2 to channel_1
+    channel_join_v1(uid2, cid1)
+
+    assert channels_list_v1(uid2['auth_user_id']) == [['ch1']]
+    assert channels_list_v1(uid2['auth_user_id']) == [['ch2']]
+
+# invalid input
+def invalid_listall_input():
+    clear_v1()
+    with pytest.raises(AccessError):
+        channels_listall_v1('BOBbob')
+
+def invalid_list_input():
+    clear_v1()
+    with pytest.raises(AccessError):
+        channels_list_v1('BOBbob')
+
+# test if listall func displays all channels including public and private
+def listall_public_and_private():
+    clear_v1()
+
+    # create user
+    uid1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+
+    # create channels 
+    cid1 = channels_create_v1(uid1['auth_user_id'], "ch1", True)
+    cid2 = channels_create_v1(uid1['auth_user_id'], "ch2", False)
+
+    listall = channels_listall_v1(uid1['auth_user_id'])
+
+    assert listall['channels'][uid1['auth_user_id']] == [['ch1']]
+    assert listall['channels'][uid1['auth_user_id']] == [['ch2']]
+

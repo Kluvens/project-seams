a87d2a8ebb5ce1032425a8139d597933e22ea268
James Cai <james.cai@student.unsw.edu.au>
2022-03-03 16:59:47 +1100

    added error testing

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,5 @@
 from src.data_store import data_store
+from src.error import AccessError
 
 def channels_list_v1(auth_user_id):
     return {
@@ -12,7 +13,11 @@ def channels_list_v1(auth_user_id):
 
 def channels_listall_v1(auth_user_id):
 
-    # if auth_user_id doesnt exist return AccessError
+    # if auth_user_id not an int return AccessError
+    try:
+        auth_user_id + 1 - 1
+    except:
+        raise AccessError
 
     data = data_store.get()
 

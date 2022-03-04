86c396611f4c6b4c89c5c81ac8dcd5a1f4283b03
Kais Alzubaidi <k.alzubaidi@student.unsw.edu.au>
2022-03-04 18:05:22 +1100

    Reverted an accidental merge into my branch

---
+++ src/channels.py
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+=======
+from src.data_store import data_store
+from src.error import AccessError
+from src.other import clear_v1
+from src.auth import auth_register_v1
+
+def channels_list_v1(auth_user_id):
+    pass
+
+def channels_listall_v1(auth_user_id):
+    return {
+        'channels': [
+            {
+                'channel_id': 1,
+                'name': 'My Channel',
+            }
+        ],
+    }
+
+def channels_create_v1(auth_user_id, name, is_public):
+    return {
+        'channel_id': 1,
+    }
+>>>>>>> Stashed changes

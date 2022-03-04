e37b6ade2c8058c44bb89e3c06cc6d230bad813a
James Cai <james.cai@student.unsw.edu.au>
2022-03-04 11:46:05 +1100

    added listv1 tests

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -2,43 +2,86 @@ import pytest
 
 from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
 from src.other import clear_v1
-from src.data_store import data_store
-from src.error import InputError, AccessError
+from src.error import AccessError
 from src.auth import auth_register_v1
 
 # =============================TESTING CORRECTNESS============================
 
-# test if listall func displays all channels including public and private
-def test_listall_public_and_private():
+# test if list func doesn't return dict when inputting user_id with
+# non-existant channel 
+def test_no_channels_joined_public():
+    clear_v1()
+
+    # create two users
+    u_id1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    u_id2 = auth_register_v1("e2@gmail.com", "abcdefg123", "Jam", "Cao")
+
+    # create private channel for user 1
+    channels_create_v1(u_id1['auth_user_id'], "ch1", True)
+
+    # return channels for user 2 (non-existant)
+    listv1 = channels_list_v1(u_id2['auth_user_id'])
+
+    # pass if trying to access non-existant channel
+    with pytest.raises(IndexError):
+        listv1['channels'][u_id2['auth_user_id']]
+
+def test_no_channels_joined_private():
+    clear_v1()
+
+    # create two users
+    u_id1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    u_id2 = auth_register_v1("e2@gmail.com", "abcdefg123", "Jam", "Cao")
+
+    # create public channel for user 1
+    channels_create_v1(u_id1['auth_user_id'], "ch1", False)
+
+    # return channels for user 2 (non-existant)
+    listv1 = channels_list_v1(u_id2['auth_user_id'])
+
+    # pass if trying to access non-existant channel 
+    with pytest.raises(IndexError):
+        listv1['channels'][u_id2['auth_user_id']]
+
+# test func output when user has joined all channels
+def test_user_join_all_channels():
     clear_v1()
 
     # create user
-    u_id = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    u_id1 = auth_register_v1("james@gmail.com", "abcdefg123", "James", "Cai")
+    u_id2 = auth_register_v1("james2@gmail.com", "abcdefg123", "Jam", "Cao")
 
-    # create channels 
-    cid1 = channels_create_v1(u_id['auth_user_id'], "ch1", True)
-    cid2 = channels_create_v1(u_id['auth_user_id'], "ch2", False)
+    # create channels
+    channels_create_v1(u_id1['auth_user_id'], "ch1", True)
+    channels_create_v1(u_id1['auth_user_id'], "ch2", False)
 
-    listall = channels_listall_v1(u_id['auth_user_id'])
+    listv1 = channels_list_v1(u_id1['auth_user_id'])
 
-    # assert listall returns filled dict
-    assert listall['channels'][u_id['auth_user_id']] == {'channel_id': [[0, 1]], 'name': [['ch1', 'ch2']]}
+    # assert user 1 is part of all channels
+    assert listv1['channels'][u_id1['auth_user_id']] == {'channel_id': [[0, 1]], 'name': [['ch1', 'ch2']]}
 
-# test listall func when no channels exist
-def test_listall_no_channels():
+# test func output when user has joined some channels
+def test_user_join_some_channels():
     clear_v1()
 
-    # create user
-    u_id = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    # create users
+    u_id1 = auth_register_v1("e1@gmail.com", "abcdefg123", "James", "Cai")
+    u_id2 = auth_register_v1("e2@gmail.com", "abcdefg1234", "Jam", "Cao")
 
-    listall = channels_listall_v1(u_id['auth_user_id'])
+    # create channels with u_id1 as owner of ch1 and ch2 and not a member
+    # of ch3
+    channels_create_v1(u_id1['auth_user_id'], "ch1", False)
+    channels_create_v1(u_id1['auth_user_id'], "ch2", True)
+    channels_create_v1(u_id2['auth_user_id'], "ch3", False)
 
-    # assert listall returns empty dict
-    assert listall['channels'][u_id['auth_user_id']] == {'channel_id': [[]], 'name': [[]]}
+    listv1 = channels_list_v1(u_id1['auth_user_id'])
+
+    # assert user 1 is part of ch1 and ch2
+    assert listv1['channels'][u_id1['auth_user_id']] == {'channel_id': [[0, 1]], 'name': [['ch1', 'ch2']]}
 
 # =============================TESTING ERRORS================================
 
-def test_invalid_listall_input():
+def test_invalid_list_input():
     clear_v1()
     with pytest.raises(AccessError):
-        channels_listall_v1('BOBbob')
+        channels_list_v1('BOBbob')

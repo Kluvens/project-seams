51018f21c5418feff416bc1b065ee08dad4c3072
James Cai <james.cai@student.unsw.edu.au>
2022-03-05 14:32:50 +1100

    changed return

--- src/channels.py
+++ src/channels.py
@@ -21,36 +21,18 @@ def channels_listall_v1(auth_user_id):
     except:
         raise AccessError
 
-    # initialise data_store
     data = data_store.get()
+    channels_list = data['channels']
+    channels_dict = {'channels' : []}
 
-    channels = data['channels']
-    channel_id_list = []
-    name_list = []
-
-    # loop through datastore and grab all channel_ids and u_ids
-    for x in range(len(channels)):
-        for y in range(len(channels[x]['all_members'])):
-            channel_idv = (channels[x]['channel_id'])
-            channel_id_list.append(channel_idv)
-            namev = (channels[x]['name'])
-            name_list.append(namev)
-
-    # convert list to string
-    listToStrc = ', '.join([str(elem) for elem in channel_id_list])
-    listToStrn = ', '.join([str(elem) for elem in name_list])
+    for channel in channels_list:
+        channel_id = channel['channel_id']
+        name = channel['name']
+        channels_dict['channels'].append({'channel_id' : channel_id, 'name' : name})
 
     data_store.set(data)
-
-    return {
-        'channels': [
-            {
-                'channel_id': listToStrc,
-                'name': listToStrn,
-            }
-        ],
-    }
-
+    
+    return channels_dict
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {

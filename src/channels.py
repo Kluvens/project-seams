fa09bff5dc778848d89b52b4c26ebe6343940ef8
James Cai <james.cai@student.unsw.edu.au>
2022-03-06 14:53:44 +1100

    added master

--- src/channels.py
+++ src/channels.py
@@ -44,8 +44,40 @@ def channels_listall_v1(auth_user_id):
 
     return channels_dict
 
-def channels_create_v1(auth_user_id, name, is_public):
+def channels_create_v1(auth_user_id, channel_name, is_public):
+    data = data_store.get()
+    
+    host_info = data["users"][auth_user_id]
+
+    host_info_list = [
+        {
+            'u_id': host_info["u_id"],
+            'email': host_info["email"],
+            'name_first': host_info["name_first"],
+            'name_last': host_info["name_last"],
+            'handle_str': host_info["handle_str"],
+        }
+    ]
+
+    channel_id = len(data["channels"])
+
+    # error
+    if len(channel_name) < 1 or len(channel_name) > 20:
+        raise InputError("length of name is less than 1 or more than 20 characters")
+
+    data["channels"].append(
+        {
+            'channel_id': channel_id,
+            'name': channel_name,
+            'is_public': is_public,
+            'owner_members': host_info_list,
+            'all_members': host_info_list,
+        }
+    )
+
+    data_store.set(data)
+
     return {
-        'channel_id': 1,
+        'channel_id': channel_id,
     }
 

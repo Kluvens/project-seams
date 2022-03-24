1340eac9a03cce46b1dacf12f109944a3d4620e6
Kais <k.alzubaidi@student.unsw.edu.au>
2022-03-25 01:23:17 +1100

    Commented out code from iteration1 tests, as the layer of testing has been lifted up to HTTP testing for iter2

--- tests/test_channel_details.py
+++ tests/test_channel_details.py
@@ -1,152 +1,152 @@
-# Jiapeng Yang(Justin) z5339252
+# # Jiapeng Yang(Justin) z5339252
 
-from src.auth import auth_register_v1, auth_login_v1
-from src.channels import channels_create_v1
-from src.channel import channel_invite_v1, channel_details_v1
-from src.other import clear_v1
+# from src.auth import auth_register_v1, auth_login_v1
+# from src.channels import channels_create_v1
+# from src.channel import channel_invite_v1, channel_details_v1
+# from src.other import clear_v1
 
-from src.error import InputError
-from src.error import AccessError
+# from src.error import InputError
+# from src.error import AccessError
 
-import pytest
+# import pytest
 
-'''
-the testing function tests when a single user created a channel
-'''
-def test_channel_details_working_single_member():
-    clear_v1()
+# '''
+# the testing function tests when a single user created a channel
+# '''
+# def test_channel_details_working_single_member():
+#     clear_v1()
 
-    # register and login for a user
-    first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
-    first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
-    first_u_id = first_auth_user["auth_user_id"]
+#     # register and login for a user
+#     first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
+#     first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
+#     first_u_id = first_auth_user["auth_user_id"]
 
-    created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
+#     created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
 
-    first_channel_details = channel_details_v1(first_u_id, created_channel_id)
+#     first_channel_details = channel_details_v1(first_u_id, created_channel_id)
 
-    # testing
-    assert first_channel_details["name"] == "Tony_channel"
-    assert first_channel_details["is_public"] == True
-    assert first_channel_details["owner_members"][0] == {
-        "u_id": first_u_id,
-        "email": "unswisgreat@unsw.edu.au",
-        "name_first": "Tony",
-        "name_last": "Stark",
-        "handle_str": "tonystark",
-    }
-    assert first_channel_details["all_members"][0] == {
-        "u_id": first_u_id,
-        "email": "unswisgreat@unsw.edu.au",
-        "name_first": "Tony",
-        "name_last": "Stark",
-        "handle_str": "tonystark",
-    }
+#     # testing
+#     assert first_channel_details["name"] == "Tony_channel"
+#     assert first_channel_details["is_public"] == True
+#     assert first_channel_details["owner_members"][0] == {
+#         "u_id": first_u_id,
+#         "email": "unswisgreat@unsw.edu.au",
+#         "name_first": "Tony",
+#         "name_last": "Stark",
+#         "handle_str": "tonystark",
+#     }
+#     assert first_channel_details["all_members"][0] == {
+#         "u_id": first_u_id,
+#         "email": "unswisgreat@unsw.edu.au",
+#         "name_first": "Tony",
+#         "name_last": "Stark",
+#         "handle_str": "tonystark",
+#     }
 
-'''
-the testing function tests when one user created a channel and invite others to join
-'''
-def test_channel_details_working_multiple_members():
-    clear_v1()
+# '''
+# the testing function tests when one user created a channel and invite others to join
+# '''
+# def test_channel_details_working_multiple_members():
+#     clear_v1()
 
-    # register and login for users
-    first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
-    first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
-    first_u_id = first_auth_user["auth_user_id"]
+#     # register and login for users
+#     first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
+#     first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
+#     first_u_id = first_auth_user["auth_user_id"]
 
-    second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
-    second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
-    second_u_id = second_auth_user["auth_user_id"]
+#     second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
+#     second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
+#     second_u_id = second_auth_user["auth_user_id"]
 
-    # create a channel
-    created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
+#     # create a channel
+#     created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
 
-    # invite other people to join the current channel
-    channel_invite_v1(first_u_id,created_channel_id,second_u_id)
+#     # invite other people to join the current channel
+#     channel_invite_v1(first_u_id,created_channel_id,second_u_id)
 
-    # get channel details
-    first_channel_details = channel_details_v1(first_u_id, created_channel_id)
+#     # get channel details
+#     first_channel_details = channel_details_v1(first_u_id, created_channel_id)
 
-    # testing
-    assert first_channel_details["name"] == "Tony_channel"
-    assert first_channel_details["is_public"] == True
-    assert first_channel_details["owner_members"][0] == {
-        "u_id": first_u_id,
-        "email": "unswisgreat@unsw.edu.au",
-        "name_first": "Tony",
-        "name_last": "Stark",
-        "handle_str": "tonystark",
-    }
-    assert first_channel_details["all_members"][0] == {
-        "u_id": first_u_id,
-        "email": "unswisgreat@unsw.edu.au",
-        "name_first": "Tony",
-        "name_last": "Stark",
-        "handle_str": "tonystark",
-    }
-    assert first_channel_details["all_members"][1] == {
-        "u_id": second_u_id,
-        "email": "hellounsw@gmail.com",
-        "name_first": "Bruce",
-        "name_last": "Banner",
-        "handle_str": "brucebanner",
-    }
+#     # testing
+#     assert first_channel_details["name"] == "Tony_channel"
+#     assert first_channel_details["is_public"] == True
+#     assert first_channel_details["owner_members"][0] == {
+#         "u_id": first_u_id,
+#         "email": "unswisgreat@unsw.edu.au",
+#         "name_first": "Tony",
+#         "name_last": "Stark",
+#         "handle_str": "tonystark",
+#     }
+#     assert first_channel_details["all_members"][0] == {
+#         "u_id": first_u_id,
+#         "email": "unswisgreat@unsw.edu.au",
+#         "name_first": "Tony",
+#         "name_last": "Stark",
+#         "handle_str": "tonystark",
+#     }
+#     assert first_channel_details["all_members"][1] == {
+#         "u_id": second_u_id,
+#         "email": "hellounsw@gmail.com",
+#         "name_first": "Bruce",
+#         "name_last": "Banner",
+#         "handle_str": "brucebanner",
+#     }
 
-'''
-testing when the users are valid but channel_id is invalid
-'''
-def test_channel_details_invalid_channel_id():
-    clear_v1()
+# '''
+# testing when the users are valid but channel_id is invalid
+# '''
+# def test_channel_details_invalid_channel_id():
+#     clear_v1()
 
-    # register and login for users
-    first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
-    first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
-    first_u_id = first_auth_user["auth_user_id"]
+#     # register and login for users
+#     first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
+#     first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
+#     first_u_id = first_auth_user["auth_user_id"]
 
-    second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
-    second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
-    second_u_id = second_auth_user["auth_user_id"]
+#     second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
+#     second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
+#     second_u_id = second_auth_user["auth_user_id"]
 
-    # create the channel and the host is the first user
-    # name is the given name
-    created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
+#     # create the channel and the host is the first user
+#     # name is the given name
+#     created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
 
-    # invite other people to join the current channel
-    channel_invite_v1(first_u_id, created_channel_id, second_u_id)
+#     # invite other people to join the current channel
+#     channel_invite_v1(first_u_id, created_channel_id, second_u_id)
 
-    # tests when channel_id is invalid
-    with pytest.raises(InputError):
-        channel_details_v1(first_u_id, created_channel_id + 1)
+#     # tests when channel_id is invalid
+#     with pytest.raises(InputError):
+#         channel_details_v1(first_u_id, created_channel_id + 1)
 
 
-'''
-testing when channel_id is valid but the user who sent the details request is not in the channel
-'''
-def test_channel_details_invalid_auth_id():
-    clear_v1()
+# '''
+# testing when channel_id is valid but the user who sent the details request is not in the channel
+# '''
+# def test_channel_details_invalid_auth_id():
+#     clear_v1()
 
-    # register and login for users
-    first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
-    first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
-    first_u_id = first_auth_user["auth_user_id"]
+#     # register and login for users
+#     first_auth_user = auth_register_v1("unswisgreat@unsw.edu.au", "unswisgreat123", "Tony", "Stark")
+#     first_auth_user = auth_login_v1("unswisgreat@unsw.edu.au", "unswisgreat123")
+#     first_u_id = first_auth_user["auth_user_id"]
 
-    second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
-    second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
-    second_u_id = second_auth_user["auth_user_id"]
+#     second_auth_user = auth_register_v1("hellounsw@gmail.com", "UNSWisgreat125", "Bruce", "Banner")
+#     second_auth_user = auth_login_v1("hellounsw@gmail.com", "UNSWisgreat125")
+#     second_u_id = second_auth_user["auth_user_id"]
 
-    third_auth_user = auth_register_v1("heyhellounsw@gmail.com", "UNSWisgreat1251", "Bruces", "Banners")
-    third_auth_user = auth_login_v1("heyhellounsw@gmail.com", "UNSWisgreat1251")
-    third_u_id = third_auth_user["auth_user_id"]
+#     third_auth_user = auth_register_v1("heyhellounsw@gmail.com", "UNSWisgreat1251", "Bruces", "Banners")
+#     third_auth_user = auth_login_v1("heyhellounsw@gmail.com", "UNSWisgreat1251")
+#     third_u_id = third_auth_user["auth_user_id"]
 
-    # create the channel and the host is the first user
-    # name is the given name
-    created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
+#     # create the channel and the host is the first user
+#     # name is the given name
+#     created_channel_id = channels_create_v1(first_u_id, "Tony_channel", True).get("channel_id")
 
-    # invite other people to join the current channel
-    channel_invite_v1(first_u_id, created_channel_id, second_u_id)
+#     # invite other people to join the current channel
+#     channel_invite_v1(first_u_id, created_channel_id, second_u_id)
 
-    # tests when auth_user_id is invalid
-    with pytest.raises(AccessError):
-        channel_details_v1(third_u_id, created_channel_id)
+#     # tests when auth_user_id is invalid
+#     with pytest.raises(AccessError):
+#         channel_details_v1(third_u_id, created_channel_id)
 
         

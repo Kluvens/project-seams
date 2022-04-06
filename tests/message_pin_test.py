cd6e89fa6f973a92b9d2aab65d1bff13ba14b9ba
Jiapeng Yang <z5339252@unsw.edu.au>
2022-04-06 21:23:31 +1000

    more on implementation

--- tests/message_pin_test.py
+++ tests/message_pin_test.py
@@ -49,27 +49,100 @@ def test_message_pin_invalid_token(setup):
     for num in range(10):
         response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
         assert response.status_code == OKAY
-    
-    # for num in range(10):
-    #     message_senddm_v1(token, dm_id, "I love you")
+        message_id = response.json()['message_id']
 
     response = requests.post(f'{url}/auth/logout/v1', json={'token': token})
     assert response.status_code == OKAY
     
-    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': 0})
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
     assert response.status_code == AccessError.code
 
-def test_message_pin_no_permission_channel():
-    pass
+def test_message_pin_no_permission_channel(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
 
-def test_message_pin_no_permission_dm():
-    pass
+    token = user1_dict['token']
+    token_second = user2_dict['token']
+    u_id_second = user2_dict['auth_user_id']
 
-def test_message_pin_invalid_message():
-    pass
+    response = requests.post(f'{url}/channel/join/v2', json={'token': token_second, 'channel_id': channel_dict['channel_id']})
+    assert response.status_code == OKAY
 
-def test_message_pin_already_pinned():
-    pass
+    response = requests.post(f'{url}/message/send/v1', json={'token': token, 'channel_id': channel_dict['channel_id'], 'message': "I love you"})
+    assert response.status_code == OKAY
+    message_id = response.json()['message_id']
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == AccessError.code
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/channel/addowner/v1', json={'token': token, 'channel_id': channel_dict['channel_id'], 'u_id': u_id_second})
+    assert response.status_code == OKAY
+
+    response = requests.get(f'{url}/channel/details/v2', params={'token': token, 'channel_id': channel_dict['channel_id']})
+    assert response.status_code == OKAY
+    details = response.json()
+    assert u_id_second in [member['u_id'] for member in details['owner_members']]
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == InputError.code
+
+# def test_message_pin_no_permission_dm(setup):
+#     user1_dict = setup[0]
+#     user2_dict = setup[1]
+#     dm_dict = setup[3]
+
+#     token = user1_dict['token']
+#     token_second = user2_dict['token']
+#     dm_id = dm_dict['dm_id']
+
+#     for num in range(10):
+#         response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+#         assert response.status_code == OKAY
+#         message_id = response.json()['message_id']
+
+#     response = requests.post(f'{url}/message/pin/v1', json={'token': token_second, 'message_id': message_id})
+#     assert response.status_code == AccessError.code
+
+
+def test_message_pin_invalid_message(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
+    
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id+100})
+    assert response.status_code == InputError.code
+
+def test_message_pin_already_pinned(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
+    
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == InputError.code
 
 def test_message_pin_working(setup):
     user1_dict = setup[0]
@@ -85,13 +158,133 @@ def test_message_pin_working(setup):
         assert response.status_code == OKAY
         message_id = response.json()['message_id']
     
-    # for num in range(10):
-    #     message_senddm_v1(token, dm_id, "I love you")
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
 
-    # response = requests.post(f'{url}/auth/logout/v1', json={'token': token})
-    # assert response.status_code == OKAY
+def test_message_unpin_invalid_token(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
     
     response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
     assert response.status_code == OKAY
 
+    response = requests.post(f'{url}/auth/logout/v1', json={'token': token})
+    assert response.status_code == OKAY
 
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == AccessError.code
+
+def test_message_unpin_already_unpinned(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
+    
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == InputError.code
+
+def test_message_unpin_no_permission(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+
+    token = user1_dict['token']
+    token_second = user2_dict['token']
+    u_id_second = user2_dict['auth_user_id']
+
+    response = requests.post(f'{url}/channel/join/v2', json={'token': token_second, 'channel_id': channel_dict['channel_id']})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/send/v1', json={'token': token, 'channel_id': channel_dict['channel_id'], 'message': "I love you"})
+    assert response.status_code == OKAY
+    message_id = response.json()['message_id']
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == AccessError.code
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == AccessError.code
+
+    response = requests.post(f'{url}/channel/addowner/v1', json={'token': token, 'channel_id': channel_dict['channel_id'], 'u_id': u_id_second})
+    assert response.status_code == OKAY
+
+    response = requests.get(f'{url}/channel/details/v2', params={'token': token, 'channel_id': channel_dict['channel_id']})
+    assert response.status_code == OKAY
+    details = response.json()
+    assert u_id_second in [member['u_id'] for member in details['owner_members']]
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == InputError.code
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token_second, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+def test_message_unpin_invalid_message(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
+    
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token, 'message_id': message_id+100})
+    assert response.status_code == InputError.code
+
+def test_message_unpin_working(setup):
+    user1_dict = setup[0]
+    user2_dict = setup[1]
+    channel_dict = setup[2]
+    dm_dict = setup[3]
+
+    token = user1_dict['token']
+    dm_id = dm_dict['dm_id']
+
+    for num in range(10):
+        response = requests.post(f'{url}/message/senddm/v1', json={'token': token, 'dm_id': dm_id, 'message': "I love you"})
+        assert response.status_code == OKAY
+        message_id = response.json()['message_id']
+    
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/unpin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY
+
+    response = requests.post(f'{url}/message/pin/v1', json={'token': token, 'message_id': message_id})
+    assert response.status_code == OKAY

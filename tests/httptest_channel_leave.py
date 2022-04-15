ed1367deb663f50f8ef5a3683ce201c67143410b
Jasmine Zeng <jasminelzeng@gmail.com>
2022-04-16 02:17:57 +1000

    UPDATE: Added input error for channel leave http tests in relation to standup

--- tests/httptest_channel_leave.py
+++ tests/httptest_channel_leave.py
@@ -1,8 +1,10 @@
-from difflib import restore
 import requests
 import pytest
 from src.config import url
 from tests.http_helpers import GenerateTestData
+from src.error import AccessError, InputError
+
+OKAY = 200
 
 #====================== Helper functions / Fixtures ===============
 
@@ -31,21 +33,30 @@ def register_test_users(num_of_users):
     dummy_data = GenerateTestData(url)
     dummy_data.register_users(num_of_users)
 
+@pytest.fixture
+def start_standup():
+    return url + 'standup/start/v1'
+
 # ===================== HTTP TESTS =====================================
 
-# invalid token type
-def test_invalid_token_type(leave_route):
-    reset_call()
-
-    response = requests.get(leave_route,json = {'token':'12345'})
-    assert response.status_code == 403 
-
 # invalid token
-def test_invalid_token(leave_route):
+def test_invalid_token(leave_route, create_route, dummy_data):
     reset_call()
 
-    response = requests.get(leave_route, json = {'token': 'hello am not a token'})
-    assert response.status_code == 403
+    user = dummy_data.register_users(num_of_users=1)[0]['token']
+
+    channel_id_obj = requests.post(create_route, json = {
+        'token':user,
+        'name': 'hello I am a channel',
+        'is_public':True,
+    })
+    channel_info = channel_id_obj.json()
+    channel_id = channel_info['channel_id']
+
+    response = requests.post(
+        leave_route, 
+        json = {'token': 'hello am not a token', 'channel_id': channel_id})
+    assert response.status_code == AccessError.code
 
 # invalid channel_id
 def test_invalid_channel_id(create_route,leave_route,dummy_data):
@@ -53,17 +64,19 @@ def test_invalid_channel_id(create_route,leave_route,dummy_data):
 
     user = dummy_data.register_users(num_of_users=1)[0]['token']
 
-    channel_id = requests.get(create_route, json = {
+    channel_id_obj = requests.post(create_route, json = {
         'token':user,
         'name': 'hello I am a channel',
         'is_public':True,
     })
+    channel_info = channel_id_obj.json()
+    channel_id = channel_info['channel_id']
 
-    response = requests.get(leave_route, json = {
+    response = requests.post(leave_route, json = {
         'token':user,
-        'channel_id': 'not a channel id'
+        'channel_id': channel_id+1000
     })
-    assert response.status_code == 403
+    assert response.status_code == InputError.code
 
 # authorized user not a member of the channel
 def test_not_user_in_channel(create_route,leave_route,dummy_data):
@@ -71,32 +84,67 @@ def test_not_user_in_channel(create_route,leave_route,dummy_data):
 
     user = dummy_data.register_users(num_of_users=1)[0]['token']
 
-    channel_id = requests.get(create_route, json = {
+    channel_id_obj = requests.post(create_route, json = {
         'token':user,
         'name': 'hello I am a channel',
         'is_public':True,
     })
+    channel_info = channel_id_obj.json()
+    channel_id = channel_info['channel_id']
 
-    response = requests.get(leave_route, json = {
+    response = requests.post(leave_route, json = {
         'token': 'i am not from this channel',
         'channel_id': channel_id
     })
-    assert response.status_code == 403
+    assert response.status_code == AccessError.code
+
+# Authorized user is starter of Active standup in channel
+def test_user_is_standup_starter(create_route,leave_route,dummy_data):
+    reset_call()
+
+    # Generate user
+    user = dummy_data.register_users(num_of_users=1)[0]
+    user_token = user['token']
+
+    # Create channel
+    channel_id_obj = requests.post(create_route, json = {
+        'token':user,
+        'name': 'hello I am a channel',
+        'is_public':True,
+    })
+    channel_info = channel_id_obj.json()
+    channel_id = channel_info['channel_id']
+
+    # Start standup in channel
+    requests.post(start_standup, json = {
+        'token': user_token,
+        'channel_id': channel_id,
+        'length': '3600'
+    })
+
+    # Leave channel
+    response = requests.post(leave_route, json = {
+        'token': user_token,
+        'channel_id': channel_id
+    })
+    assert response.status_code == InputError.code
 
 # routine behavior
-def working_setup(create_route,leave_route,dummy_data):
+def test_working_setup(create_route,leave_route,dummy_data):
     reset_call()
 
     user = dummy_data.register_users(num_of_users=1)[0]['token']
 
-    channel_id = requests.get(create_route, json = {
+    channel_id_obj = requests.post(create_route, json = {
         'token':user,
         'name': 'hello I am a channel',
         'is_public':True,
     })
+    channel_info = channel_id_obj.json()
+    channel_id = channel_info['channel_id']
 
-    response = requests.get(leave_route, json = {
+    response = requests.post(leave_route, json = {
         'token': user,
         'channel_id': channel_id
     })
-    assert response.status_code == 200
+    assert response.status_code == OKAY

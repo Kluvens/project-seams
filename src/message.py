7e192835923d0ae079ee53fc8b910cc60b51c281
Jasmine Zeng <jasminelzeng@gmail.com>
2022-04-17 03:22:39 +1000

    FIXED: changed return type for message share to include key

--- src/message.py
+++ src/message.py
@@ -314,4 +314,4 @@ def message_share_v1(token,og_message_id,message,channel_id,dm_id):
         # Send new message
         shared_message_id = message_senddm_v1(token, dm_id,new_message)
 
-    return {shared_message_id}
+    return {'shared_message_id': shared_message_id}

849e19e0318d90e9343168ae22e8bce18989dfe8
Jasmine Zeng <jasminelzeng@gmail.com>
2022-04-28 19:56:21 +1000

    ONHOLD

--- tests/message_share_http_test.py
+++ tests/message_share_http_test.py
@@ -56,130 +56,130 @@ def dm_channel(setup_dict):
 
 # ======================================= HTTP TESTS =========================================================
 
-# input: Invalid channel 
-def test_invalid_channel(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = dm_channel(setup_dict)["message_id"]
-    response = message_share_v1_http(token,og_message_id,'invalid channel test','12345','-1')
-    assert response.status_code == InputError.code
+# # input: Invalid channel 
+# def test_invalid_channel(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = dm_channel(setup_dict)["message_id"]
+#     response = message_share_v1_http(token,og_message_id,'invalid channel test','12345','-1')
+#     assert response.status_code == InputError.code
 
-# input: Invalid dm 
-def test_invalid_dm(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    response = message_share_v1_http(token,og_message_id,'invalid dm test','-1','12345')
-    assert response.status_code == InputError.code
+# # input: Invalid dm 
+# def test_invalid_dm(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     response = message_share_v1_http(token,og_message_id,'invalid dm test','-1','12345')
+#     assert response.status_code == InputError.code
 
-# input: Both channel and dm id is not -1
-def test_neither_neg1_valid_ids(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    channel_id = setup_dict['channel_id']
-    dm_id = setup_dict['dm_id']
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    response = message_share_v1_http(token,og_message_id,'neither -1,both valid',channel_id,dm_id)
-    assert response.status_code == InputError.code 
+# # input: Both channel and dm id is not -1
+# def test_neither_neg1_valid_ids(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     channel_id = setup_dict['channel_id']
+#     dm_id = setup_dict['dm_id']
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     response = message_share_v1_http(token,og_message_id,'neither -1,both valid',channel_id,dm_id)
+#     assert response.status_code == InputError.code 
 
-def test_neither_neg1_invalid_ids(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    response = message_share_v1_http(token,og_message_id,'neither -1,neither valid','123','456')
-    assert response.status_code == InputError.code 
+# def test_neither_neg1_invalid_ids(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     response = message_share_v1_http(token,og_message_id,'neither -1,neither valid','123','456')
+#     assert response.status_code == InputError.code 
 
-# input: Invalid message id
-def test_invalid_message_id_channeltodm(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    dm_id = setup_dict['dm_id']
-    response = message_share_v1_http(token, 12345,'invalid message id','-1',dm_id)
-    assert response.status_code == InputError.code
+# # input: Invalid message id
+# def test_invalid_message_id_channeltodm(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     dm_id = setup_dict['dm_id']
+#     response = message_share_v1_http(token, 12345,'invalid message id','-1',dm_id)
+#     assert response.status_code == InputError.code
 
-def test_invalid_message_id_dmtochannel(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = dm_channel(setup_dict)["message_id"]
-    channel_id = setup_dict['channel_id']
+# def test_invalid_message_id_dmtochannel(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = dm_channel(setup_dict)["message_id"]
+#     channel_id = setup_dict['channel_id']
 
-    response = message_share_v1_http(token, -12345, 'invalid message id',channel_id,'-1')
-    assert response.status_code == InputError.code
+#     response = message_share_v1_http(token, -12345, 'invalid message id',channel_id,'-1')
+#     assert response.status_code == InputError.code
 
-# input: Message length > 1000
-@pytest.fixture
-def message():
-    return '''Thus Spoke Zarathustra A Book for All and None
-    When Zarathustra was thirty years old he left his home and the lake of his home 
-    and went into the mountains. Here he enjoyed his spirit and his solitude and for 
-    ten years he did not tire of it. But at last his heart transformed,  one morning 
-    he arose with the dawn, stepped before the sun and spoke thus to it: 
-    “You great star! What would your happiness be if you had not those for whom you shine 
-    For ten years you have come up here to my cave: you would have tired of your light 
-    and of this route without me, my eagle and my snake. But we awaited you every morning, 
-    took your overflow from you and blessed you for it. Behold! I am weary of my wisdom, 
-    like a bee that has gathered too much honey. I need hands that reach out. 
-    I want to bestow and distribute until the wise among human beings have once again 
-    enjoyed their folly, and the poor once again their wealth. For this I must descend into
-    the depths, as you do evenings when you go behind the sea and bring light even to the 
-    underworld, you super-rich star! Like you, I must go down as the human beings say, 
-    to whom I want to descend. So bless me now, you quiet eye that can look upon even an 
-    all too great happiness without envy! Bless the cup that wants to flow over, such that 
-    water flows golden from it and everywhere carries the reflection of your bliss! 
-    Behold! This cup wants to become empty again, and Zarathustra wants to become human again.”
-    Thus began Zarathustras going under.'''
+# # input: Message length > 1000
+# @pytest.fixture
+# def message():
+#     return '''Thus Spoke Zarathustra A Book for All and None
+#     When Zarathustra was thirty years old he left his home and the lake of his home 
+#     and went into the mountains. Here he enjoyed his spirit and his solitude and for 
+#     ten years he did not tire of it. But at last his heart transformed,  one morning 
+#     he arose with the dawn, stepped before the sun and spoke thus to it: 
+#     “You great star! What would your happiness be if you had not those for whom you shine 
+#     For ten years you have come up here to my cave: you would have tired of your light 
+#     and of this route without me, my eagle and my snake. But we awaited you every morning, 
+#     took your overflow from you and blessed you for it. Behold! I am weary of my wisdom, 
+#     like a bee that has gathered too much honey. I need hands that reach out. 
+#     I want to bestow and distribute until the wise among human beings have once again 
+#     enjoyed their folly, and the poor once again their wealth. For this I must descend into
+#     the depths, as you do evenings when you go behind the sea and bring light even to the 
+#     underworld, you super-rich star! Like you, I must go down as the human beings say, 
+#     to whom I want to descend. So bless me now, you quiet eye that can look upon even an 
+#     all too great happiness without envy! Bless the cup that wants to flow over, such that 
+#     water flows golden from it and everywhere carries the reflection of your bliss! 
+#     Behold! This cup wants to become empty again, and Zarathustra wants to become human again.”
+#     Thus began Zarathustras going under.'''
 
-def test_message_over_1000_channeltodm(setup, message):
+# def test_message_over_1000_channeltodm(setup, message):
     
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    dm_id = setup_dict['dm_id']
-    response = message_share_v1_http(token,og_message_id,message,'-1',dm_id)
-    assert response.status_code == InputError.code
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     dm_id = setup_dict['dm_id']
+#     response = message_share_v1_http(token,og_message_id,message,'-1',dm_id)
+#     assert response.status_code == InputError.code
 
-def test_message_over_1000_dmtochannel(setup, message):
+# def test_message_over_1000_dmtochannel(setup, message):
     
-    setup_dict = setup
-    token = setup_dict['tokens'][1]
-    og_message_id = dm_channel(setup_dict)["message_id"]
-    channel_id = setup_dict['channel_id']
+#     setup_dict = setup
+#     token = setup_dict['tokens'][1]
+#     og_message_id = dm_channel(setup_dict)["message_id"]
+#     channel_id = setup_dict['channel_id']
 
-    response = message_share_v1_http(token,og_message_id,message,channel_id,-1)
-    assert response.status_code == InputError.code
+#     response = message_share_v1_http(token,og_message_id,message,channel_id,-1)
+#     assert response.status_code == InputError.code
 
-# access: Auth user not part of channel/dm they are sharing TO
-def test_user_not_member_channeltodm(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][0]
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    dm_id = setup_dict['dm_id']
-    response = message_share_v1_http(token,og_message_id,'not member of dm to share to',-1,dm_id)
-    assert response.status_code == AccessError.code
+# # access: Auth user not part of channel/dm they are sharing TO
+# def test_user_not_member_channeltodm(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][0]
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     dm_id = setup_dict['dm_id']
+#     response = message_share_v1_http(token,og_message_id,'not member of dm to share to',-1,dm_id)
+#     assert response.status_code == AccessError.code
 
-def test_user_not_member_dmtochannel(setup):
-    setup_dict = setup
-    token = setup_dict['tokens'][0]
-    og_message_id = dm_channel(setup_dict)["message_id"]
-    channel_id = setup_dict['channel_id']
-    response = message_share_v1_http(token,og_message_id,'not member of channel to share to',channel_id, -1)
-    assert response.status_code == AccessError.code
+# def test_user_not_member_dmtochannel(setup):
+#     setup_dict = setup
+#     token = setup_dict['tokens'][0]
+#     og_message_id = dm_channel(setup_dict)["message_id"]
+#     channel_id = setup_dict['channel_id']
+#     response = message_share_v1_http(token,og_message_id,'not member of channel to share to',channel_id, -1)
+#     assert response.status_code == AccessError.code
 
-# access: Invalid Token
-def test_invalid_token_channeltodm(setup):
-    setup_dict = setup
-    og_message_id = channel_dm(setup_dict)["message_id"]
-    dm_id = setup_dict['dm_id']
-    response = message_share_v1_http('abcde',og_message_id,'invalid token, channel/dm',-1,dm_id)
-    assert response.status_code == AccessError.code 
+# # access: Invalid Token
+# def test_invalid_token_channeltodm(setup):
+#     setup_dict = setup
+#     og_message_id = channel_dm(setup_dict)["message_id"]
+#     dm_id = setup_dict['dm_id']
+#     response = message_share_v1_http('abcde',og_message_id,'invalid token, channel/dm',-1,dm_id)
+#     assert response.status_code == AccessError.code 
 
-def test_invalid_token_dmtochannel(setup):
-    setup_dict = setup
-    og_message_id = dm_channel(setup_dict)["message_id"]
-    channel_id = setup_dict['channel_id']
-    response = message_share_v1_http('abcde',og_message_id,'invalid token, channel/dm',channel_id, -1)
-    assert response.status_code == AccessError.code 
+# def test_invalid_token_dmtochannel(setup):
+#     setup_dict = setup
+#     og_message_id = dm_channel(setup_dict)["message_id"]
+#     channel_id = setup_dict['channel_id']
+#     response = message_share_v1_http('abcde',og_message_id,'invalid token, channel/dm',channel_id, -1)
+#     assert response.status_code == AccessError.code 
 
 
 # # Routine Behavior 
